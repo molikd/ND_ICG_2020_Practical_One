2d9989a3b74f883b93048114ec1ee3888da1fc9a
Simon Weaver <sweaver4@crcfe01.crc.nd.edu>
2020-08-15 11:03:51 -0400

    drafts for answers 2-4

--- practical.sh
+++ practical.sh
@@ -3,14 +3,17 @@
 # Best of luck!
 
 # 1. Output to the command line the branch and weather or not your branch is up to date with the master branch. 
-echo -e "\nOutput the branch and any files that have been modified from master:\n"
+echo -e "\nOutput the branch and any files that have not been modified and not committed:\n"
 git status -uno
 
 # 2. List every commit, but only the last two changes in each commit. 
-echo -e "\nAnswer Two\n"
+echo -e "\nAll of the commits, and the last two changes in each commit:\n"
+git log  --pretty=short
 
 # 3. List all branches that have not been merged in the current repository. 
-echo -e "\nAnswer Three\n"
+echo -e "\nAll branches that have not been merged:\n"
+git branch --no-merged
 
 # 4. List the author of the first line of practical.sh
-echo -e "\nAnswer Four\n"
+echo -e "\nThe first line of practical.sh along with who wrote it and when it was written:\n"
+git blame -L 1,1 practical.sh
