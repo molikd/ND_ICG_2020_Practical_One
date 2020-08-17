1feb48b50b29fc50d5af13947f1d2948e748b880
Mae <merimaat1@gmail.com>
2020-08-17 05:25:28 -0400

    Adding initial solutions, test commit

--- practical.sh
+++ practical.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Best of luck!
-
 # 1. Output to the command line the branch and weather or not your branch is up to date with the master branch. 
 echo -e "\nAnswer One\n"
+git status
 
 # 2. List every commit, but only the last two changes in each commit. 
 echo -e "\nAnswer Two\n"
+git log --pretty=oneline
+#git log --pretty=oneline -2
 
 # 3. List all branches that have not been merged in the current repository. 
 echo -e "\nAnswer Three\n"
+#List branches that have not been merged into the current head
+git branch --no-merged
+#List branches that have not been merged into master
+#git branch --no-merged master
 
 # 4. List the author of the first line of practical.sh
 echo -e "\nAnswer Four\n"
+git blame -L 1,1 practical.sh
