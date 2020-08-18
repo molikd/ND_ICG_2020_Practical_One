03c31db073b04607ed941e267bec805258eb3812
Seth VanDexter <svandext@crcfe01.crc.nd.edu>
2020-08-17 23:42:54 -0400

    first commit

--- practical.sh
+++ practical.sh
@@ -2,14 +2,14 @@
 
 # Best of luck!
 
-# 1. Output to the command line the branch and weather or not your branch is up to date with the master branch. 
-echo -e "\nAnswer One\n"
+# 1. Output to the command line the branch and whether or not your branch is up to date with the master branch. 
+echo -e "git status"
 
 # 2. List every commit, but only the last two changes in each commit. 
-echo -e "\nAnswer Two\n"
+echo -e "git log -2"
 
 # 3. List all branches that have not been merged in the current repository. 
-echo -e "\nAnswer Three\n"
+echo -e "git branch --no-merged master"
 
 # 4. List the author of the first line of practical.sh
-echo -e "\nAnswer Four\n"
+echo -e "git blame | head -1"
