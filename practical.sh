434a90a1340ebb5e428d352ee01be7bd7ea032ee
Meghana Machireddy <mmachire@crcfe01.crc.nd.edu>
2020-08-18 10:51:30 -0400

    assignment 1

--- practical.sh
+++ practical.sh
@@ -3,13 +3,30 @@
 # Best of luck!
 
 # 1. Output to the command line the branch and weather or not your branch is up to date with the master branch. 
-echo -e "\nAnswer One\n"
+echo -e "\ncurrent branch is master\n"
 
 # 2. List every commit, but only the last two changes in each commit. 
-echo -e "\nAnswer Two\n"
+echo -e "\ncommit 74313b9de96c415b9032f8dab95e3a13203154e5
+    Merge pull request #1 from molikd/dmolik
+    Best of Luck to you
+
+commit bf83a67e41431a35904bd3a894372433b155e74b
+    Best of Luck to you
+
+commit 788c1aeadcfcb0b08d45218ffa29bf8887c4708c
+    Update README.md
+    
+commit d26be6c3e5a7b973ddd90199c17c9a52d1f9f759
+    Create practical.sh
+
+commit 8c27e53018ac730fc7801de579e30b100e10b839
+    Update README.md
+
+commit cda7b38a3da71c9432e863bbca71b40aec580bc1
+    Initial commit\n"
 
 # 3. List all branches that have not been merged in the current repository. 
-echo -e "\nAnswer Three\n"
+echo -e "\nThere are no branches that have not been merged in the current repository\n"
 
 # 4. List the author of the first line of practical.sh
-echo -e "\nAnswer Four\n"
+echo -e "\nDavid Molik\n"
