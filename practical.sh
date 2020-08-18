c5409a392ed9d807f5d15afe4f476624235aff88
Seth VanDexter <svandext@crcfe01.crc.nd.edu>
2020-08-17 23:55:37 -0400

    second commit

--- practical.sh
+++ practical.sh
@@ -3,13 +3,13 @@
 # Best of luck!
 
 # 1. Output to the command line the branch and whether or not your branch is up to date with the master branch. 
-echo -e "git status"
+echo -e "\ngit status\n"
 
 # 2. List every commit, but only the last two changes in each commit. 
-echo -e "git log -2"
+echo -e "\ngit log -2\n"
 
 # 3. List all branches that have not been merged in the current repository. 
-echo -e "git branch --no-merged master"
+echo -e "\ngit branch --no-merged master\n"
 
 # 4. List the author of the first line of practical.sh
-echo -e "git blame | head -1"
+echo -e "\ngit blame | awk '{ print $2, $3 }' | head -1 head -1\n"
