39f76fd97a8728ee5963858c9e0c89ebb11283ab
Amanda Driscoe <amandadriscoe@Rose.local>
2020-09-17 23:12:22 -0400

    adding practical

--- practical.sh
+++ practical.sh
@@ -3,13 +3,13 @@
 # Best of luck!
 
 # 1. Output to the command line the branch and weather or not your branch is up to date with the master branch. 
-echo -e "\nAnswer One\n"
+echo -e "\ngit status\n"
 
 # 2. List every commit, but only the last two changes in each commit. 
-echo -e "\nAnswer Two\n"
+echo -e "\ngit log -2\n"
 
 # 3. List all branches that have not been merged in the current repository. 
-echo -e "\nAnswer Three\n"
+echo -e "\ngit branch --no-merged master\n"
 
 # 4. List the author of the first line of practical.sh
-echo -e "\nAnswer Four\n"
+echo -e "\ngit blame -L 1,1 practical.sh\n"
