34d879765749606d8c13c7e290e8a70dbe3b4dc1
Meghana Machireddy <mmachire@crcfe01.crc.nd.edu>
2020-11-17 17:22:42 -0500

    submission

--- practical.sh
+++ practical.sh
@@ -3,30 +3,24 @@
 # Best of luck!
 
 # 1. Output to the command line the branch and weather or not your branch is up to date with the master branch. 
-echo -e "\ncurrent branch is master\n"
+echo -e "\non branch master\n"
 
 # 2. List every commit, but only the last two changes in each commit. 
-echo -e "\ncommit 74313b9de96c415b9032f8dab95e3a13203154e5
+echo -e "\nDate:   Tue Aug 18 10:51:30 2020 -0400
+
+    assignment 1
+
+commit 74313b9de96c415b9032f8dab95e3a13203154e5
+Merge: 788c1ae bf83a67
+Author: David Molik <dave@d3fy.net>
+Date:   Thu Aug 13 14:24:55 2020 -0400
+
     Merge pull request #1 from molikd/dmolik
-    Best of Luck to you
 
-commit bf83a67e41431a35904bd3a894372433b155e74b
-    Best of Luck to you
-
-commit 788c1aeadcfcb0b08d45218ffa29bf8887c4708c
-    Update README.md
-    
-commit d26be6c3e5a7b973ddd90199c17c9a52d1f9f759
-    Create practical.sh
-
-commit 8c27e53018ac730fc7801de579e30b100e10b839
-    Update README.md
-
-commit cda7b38a3da71c9432e863bbca71b40aec580bc1
-    Initial commit\n"
+    Best of Luck to you\n"
 
 # 3. List all branches that have not been merged in the current repository. 
-echo -e "\nThere are no branches that have not been merged in the current repository\n"
+echo -e "\n*mmachire\n"
 
 # 4. List the author of the first line of practical.sh
-echo -e "\nDavid Molik\n"
+echo -e "\nd26be6c3 (David Molik 2020-08-12 16:52:45 -0400 1) #!/bin/bash\n"
